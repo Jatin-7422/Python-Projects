9dab1bdf0176e7607ecaaf68e17cb2e4d26b676d
Jatin Kumar <jatinkumar7422@gmail.com>
2024-01-06 20:03:48 +0530

    Main file of the video to audio downloader

--- Video_To_Audio_Downloader/videotoaudio.py
+++ Video_To_Audio_Downloader/videotoaudio.py
@@ -1,7 +1,19 @@
-from pytube import YouTube
-import pytube
-import os
+import string
 
+from pytube import YouTube
+import os
+import shutil
+import time
+
+def download_video(video_url, path):
+    yt = YouTube(video_url)
+    video_stream = yt.streams.filter(only_audio=True).first()
+
+    if video_stream:
+        video_stream.download(output_path=path, filename=yt.video_id + '.mp4')
+        return True
+    else:
+        return False
 
 def main():
     video_url = input('Enter YouTube video URL: ')
@@ -11,16 +23,33 @@ def main():
     else:
         path = os.getcwd() + '/'
 
-    name = pytube.extract.video_id(video_url)
-    YouTube(video_url).streams.filter(only_audio=True).first().download(filename=name)
-    location = path + name + '.mp4'
-    renametomp3 = path + name + '.mp3'
+    download_successful = download_video(video_url, path)
 
-    if os.name == 'nt':
-        os.system('ren {0} {1}'.format(location, renametomp3))
+    if download_successful:
+        video_id = YouTube(video_url).video_id
+        original_file_path = path + video_id + '.mp4'
+
+        # Wait until the file is fully downloaded (check every second)
+        while not os.path.exists(original_file_path):
+            time.sleep(1)
+
+        try:
+            # Get the video title and use it for renaming
+            video_title = YouTube(video_url).title
+            # Remove invalid characters from the filename
+            valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+            video_title_cleaned = ''.join(c for c in video_title if c in valid_chars)
+            audio_file_path = path + f'{video_title_cleaned}.mp3'
+
+            # Use shutil.move for a safer file move operation
+            shutil.move(original_file_path, audio_file_path)
+            print(f'Conversion completed: {audio_file_path}')
+        except FileNotFoundError:
+            print('File not found error during the move operation.')
+        except Exception as e:
+            print(f'An error occurred during the move operation: {e}')
     else:
-        os.system('mv {0} {1}'.format(location, renametomp3))
-
+        print('No available audio stream for the given video.')
 
 if __name__ == '__main__':
     main()
